a8d7fbf0d01566ac9f81b8b49837f8850a489dac
Se7enSe7enSe7en <jedden.mendoza@gmail.com>
2025-06-28 22:15:19 +0800

    build: redo django start project

--- server/urls.py
+++ server/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for DentalSystem project.
+URL configuration for server project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
