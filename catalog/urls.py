bd7feec309158894349cbd0a541bf4d156efd0d6
Se7enSe7enSe7en <jedden.mendoza@gmail.com>
2025-07-26 19:40:41 +0800

    wip: setup urls for base html

---
+++ catalog/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+from .views import *
+
+urlpatterns = [
+    path('', CatalogView.as_view(), name="catalog_list_url" )
+]
--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views.generic import View
 
 # Create your views here.
+class CatalogView(View):
+    def get(self, request):
+        return render(request, 'catalog/catalog.html', context={'name': "RED"})
